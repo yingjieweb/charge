d56516a06d2661a17bd712fd8a47bc8d34d43db6
yingjieweb <1184061078@qq.com>
2020-04-23 17:30:27 +0800

    add router

---
+++ src/views/Labels.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    labels
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Labels"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Money.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    money
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "money"
+  }
+</script>
+
+<style scoped>
+
+</style>
