4b3f5ffb1c3ba76f1775b9b5ffa13aa240478df4
yingjieweb <1184061078@qq.com>
2020-04-23 21:21:33 +0800

    add 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="sorry">
+    <span id="tipNum">{{count}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NotFound",
+    data() {
+      return {
+        targetUrl: "https://yingjieweb.github.io/navigator/dist/index.html",  //倒计时结束跳转页面
+        count: 9,
+      }
+    },
+    mounted() {
+      this.load()
+    },
+    methods: {
+      load() {
+        setInterval(()=>{
+          if (this.count !== 0){
+            this.count --
+          }else {
+            window.location.href = this.targetUrl;
+            window.clearInterval(this.timeout);
+          }
+        },900)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .top_link {
+    height: 50px;
+    width: 150px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+
+  .top_link a {
+    text-decoration: none;
+    display: block;
+    height: 50px;
+    width: 150px;
+  }
+
+  .top_link a:hover {
+    text-decoration: none;
+  }
+
+  .sorry {
+    height: 530px;
+    background-image: url(http://www.qijishow.com/img/sorry.jpg);
+    background-repeat: no-repeat;
+    background-position: 96px 84px;
+    width: 524px;
+    margin-right: auto;
+    margin-left: auto;
+    font-family: Impact;
+    font-size: 400px;
+    color: #BDBDBD;
+    line-height: 460px;
+    padding-top: 338px;
+    padding-left: 332px;
+  }
+
+  .statistics {
+    width: 950px;
+    margin-right: auto;
+    margin-left: auto;
+    height: 15px;
+    font-family: Verdana;
+    font-size: 11px;
+    color: #ecedef;
+    text-align: right;
+  }
+
+  .statistics a {
+    color: #ecedef;
+    text-decoration: none;
+  }
+
+  .statistics a:hover {
+    text-decoration: none;
+  }
+</style>
