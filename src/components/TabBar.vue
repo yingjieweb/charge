38ca802e04b3e4caad24a1b616121e9a68e2c157
yingjieweb <1184061078@qq.com>
2020-04-25 19:00:21 +0800

    路由激活

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="tab-bar">
-    <router-link to="/money" class="tab-bar-item">
+    <router-link to="/money" class="tab-bar-item" active-class="selected">
       <Icon name="money"></Icon>
       记账
     </router-link>
-    <router-link to="/labels" class="tab-bar-item">
+    <router-link to="/labels" class="tab-bar-item" active-class="selected">
       <Icon name="label"></Icon>
       标签
     </router-link>
-    <router-link to="/statistics" class="tab-bar-item">
+    <router-link to="/statistics" class="tab-bar-item" active-class="selected">
       <Icon name="statistics"></Icon>
       统计
     </router-link>
@@ -33,6 +33,9 @@
       align-items: center;
       flex-direction: column;
       padding: 2px 0;
+    }
+    .selected{
+      color: #00BFA6;
     }
   }
 </style>
