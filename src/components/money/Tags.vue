1560a8ab3ad8fceb9c6ccc2a13fbc9a48d94afe1
yingjieweb <1184061078@qq.com>
2020-04-27 09:01:21 +0800

    Money模块组件化

---
+++ src/components/money/Tags.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tags">
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Tags'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .tags {
+    font-size: 14px;
+    padding: 16px;
+    height: calc(100vh - 537px);
+    overflow: auto;
+    > .current {
+      display: flex;
+      flex-wrap: wrap;
+      > li {
+        $h: 24px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        background: #d9d9d9;
+        padding: 0 16px;
+        margin-right: 12px;
+        margin-top: 4px;
+      }
+    }
+    > .new {
+      padding-top: 16px;
+      button {
+        background: transparent;
+        border: none;
+        color: #999;
+        border-bottom: 1px solid;
+        padding: 0 4px;
+      }
+    }
+  }
+</style>
