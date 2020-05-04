b6e624294df834d4d411309002835f02d5dfb831
yingjieweb <1184061078@qq.com>
2020-05-04 21:38:00 +0800

    ts重构money组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -10,18 +10,21 @@
 </template>
 
 <script lang="ts">
+  import Vue from 'vue'
   import Tags from "@/components/money/Tags.vue";
   import Notes from "@/components/money/Notes.vue";
   import Types from "@/components/money/Types.vue";
   import NumberPad from "@/components/money/NumberPad.vue";
-  export default {
-    name: "Money",
-    components: {NumberPad, Types, Notes, Tags},
-    data() {
-      return {
-        title: 'Money',
-        tags: ['衣','食','住','行']
-      }
+  import {Component} from "vue-property-decorator";
+
+  @Component({
+    components: {NumberPad, Types, Notes, Tags}
+  })
+  export default class Money extends Vue{
+    title = 'Money';
+    tags = ['衣','食','住','行'];
+    yyy(){
+      console.log(111)
     }
   }
 </script>
