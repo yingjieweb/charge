24ae60e096e181a703d9896d831b85aec1a1da05
yingjieweb <1184061078@qq.com>
2020-05-07 19:22:17 +0800

    Layout组件导航插槽封装

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="layout">
     <div class="nav">
-      {{title}}
+      <div class="nav-left"><slot name="navLeft"></slot></div>
+      <div class="nav-center">{{title}}</div>
+      <div class="nav-right"><slot name="navRight"></slot></div>
     </div>
     <div class="content">
       <slot name="content"></slot>
@@ -32,6 +34,19 @@
     text-align: center;
     background-color: $main-color;
     color: white;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .nav-left, .nav-right{
+    width: 20px;
+    margin-left: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .nav-center{
+    flex: 1;
   }
   .content{
     flex-grow: 1;
