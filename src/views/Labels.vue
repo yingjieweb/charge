9b06744adedef0545b90f77f2b253b39675f79b7
yingjieweb <1184061078@qq.com>
2020-05-10 21:53:41 +0800

    labels样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,22 +1,59 @@
 <template>
   <Layout :title="'Labels navigator'">
     <template v-slot:content>
-      Labels 页面
+      <router-link class="tag" v-for="tag in tags" :key="tag" :to="`/labels/edit/${tag.id}`">
+        <span>{{tag}}</span>
+        <Icon name="right"/>
+      </router-link>
+      <div class="createTag-wrapper">
+        <Button class="createTag">
+          新建标签
+        </Button>
+      </div>
     </template>
   </Layout>
 </template>
 
 <script lang="ts">
-  export default {
-    name: "Labels",
-    data(){
-      return{
-        title: 'Labels'
-      }
-    }
+  import Vue from 'vue'
+  import {Component} from "vue-property-decorator";
+
+  @Component
+  export default class Labels extends Vue{
+    tags = JSON.parse(window.localStorage.getItem('tags')!);
   }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .tags {
+    background: white;
+    font-size: 16px;
+    padding-left: 16px;
+    > .tag {
+      min-height: 44px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1px solid #e6e6e6;
+      svg {
+        width: 18px;
+        height: 18px;
+        color: #666;
+        margin-right: 16px;
+      }
+    }
+  }
+  .createTag {
+    background: #767676;
+    color: white;
+    border-radius: 4px;
+    border: none;
+    height: 40px;
+    padding: 0 16px;
+    &-wrapper {
+      text-align: center;
+      padding: 16px;
+      margin-top: 44-16px;
+    }
+  }
 </style>
