c87e0ae3636cf215cbb877e08f24cdf0fdf21088
yingjieweb <1184061078@qq.com>
2020-05-11 16:09:11 +0800

    labels样式调整

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,12 @@
 <template>
   <Layout :title="'Labels navigator'">
     <template v-slot:content>
-      <router-link class="tag" v-for="tag in tags" :key="tag" :to="`/labels/edit/${tag.id}`">
-        <span>{{tag}}</span>
-        <Icon name="right"/>
-      </router-link>
+      <div class="tags">
+        <router-link class="tag" v-for="tag in tags" :key="tag" :to="`/labels/edit/${tag.id}`">
+          <span>{{tag}}</span>
+          <Icon name="right"/>
+        </router-link>
+      </div>
       <div class="createTag-wrapper">
         <Button class="createTag">
           新建标签
@@ -26,14 +28,13 @@
 
 <style lang="scss" scoped>
   .tags {
-    background: white;
     font-size: 16px;
     padding-left: 16px;
     > .tag {
       min-height: 44px;
       display: flex;
-      align-items: center;
       justify-content: space-between;
+      align-items: center;
       border-bottom: 1px solid #e6e6e6;
       svg {
         width: 18px;
@@ -44,16 +45,16 @@
     }
   }
   .createTag {
-    background: #767676;
     color: white;
-    border-radius: 4px;
     border: none;
+    background: #767676;
+    border-radius: 4px;
     height: 40px;
     padding: 0 16px;
     &-wrapper {
       text-align: center;
       padding: 16px;
-      margin-top: 44-16px;
+      margin-top: 28px;
     }
   }
 </style>
