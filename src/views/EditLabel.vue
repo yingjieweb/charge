cb522f7552486312ca3a8e92b2107747f6f502c5
yingjieweb <1184061078@qq.com>
2020-05-12 18:57:54 +0800

    编辑tag页面及路由

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout :title="'Edit Label'">
+    <div>这是编辑标签页面</div>
+  </Layout>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import {Component} from "vue-property-decorator";
+
+  @Component
+  export default class EditLabel extends Vue{
+    created(){
+      console.log(this.$route.params);
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
