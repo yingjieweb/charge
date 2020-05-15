3fa288cb6ef8dc4cd813c22721878472f610b26e
yingjieweb <1184061078@qq.com>
2020-05-15 22:27:10 +0800

    封装全局Button组件

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,14 @@ import store from './store'
 import TabBar from '@/components/TabBar.vue'
 import Layout from '@/components/Layout.vue'
 import Icon from '@/components/Icon.vue'
+import Button from "@/components/common/Button.vue";
 
 Vue.config.productionTip = false
 
 Vue.component('TabBar',TabBar)  //全局TabBar
 Vue.component('Layout',Layout)  //全局Layout
 Vue.component('Icon',Icon)  //全局Icon
+Vue.component('Button', Button) //全局按钮
 
 new Vue({
   router, //router: router
