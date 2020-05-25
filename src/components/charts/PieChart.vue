456e568e308b753b8d82063f2d1d21cb22ec15ca
yingjieweb <1184061078@qq.com>
2020-05-25 11:04:10 +0800

    数据可视化饼图

--- src/components/charts/PieChart.vue
+++ src/components/charts/PieChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pieChart" :style="{width: `${width}px`, height: `${height}px`}">
+  <div class="pieChart">
     <div ref="pieChart" style="width: 100%; height: 100%;"></div>
   </div>
 </template>
@@ -7,56 +7,68 @@
 <script lang="ts">
   import Vue from 'vue';
   import echarts from 'echarts'
-  import {Component} from "vue-property-decorator";
+  import {Component, Prop, Watch} from "vue-property-decorator";
 
   @Component
-  export default class PieChart extends Vue{
+  export default class PieChart extends Vue {
+    @Prop() readonly dataSource!: [];
+
     width = 0;
     height = 0;
+    option = {
+      tooltip: {
+        trigger: 'item',
+        formatter: '{b}: {c}￥ ({d}%)'
+      },
+      series: [
+        {
+          type: 'pie',
+          radius: ['50%', '70%'],
+          avoidLabelOverlap: false,
+          label: {
+            normal: {
+              position: 'outer',
+              show: true
+            }
+          },
+          data: [
+            ...this.dataSource
+          ]
+        }
+      ]
+    };
 
-    created(){
+    created() {
       this.width = document.documentElement.clientWidth;
       this.height = document.documentElement.clientWidth * 0.54;
     }
-    mounted(){
+
+    mounted() {
       this.init();
     }
 
-    init(){
+    init() {
+      const Chart = echarts.init(this.$refs.pieChart as HTMLCanvasElement);
+      Chart.setOption(this.option)
+    }
+
+    @Watch('dataSource')
+    onDataSourceChanged() {
       const Chart = echarts.init(this.$refs.pieChart as HTMLCanvasElement);
       Chart.setOption({
-        series: [
-          {
-            name: '访问来源',
-            type: 'pie',
-            radius: ['50%', '70%'],
-            avoidLabelOverlap: false,
-            label: {
-              show: false,
-              position: 'center'
-            },
-            labelLine: {
-              normal: {
-                show: true
-              }
-            },
-            data: [
-              {value: 335, name: '直接访问'},
-              {value: 310, name: '邮件营销'},
-              {value: 234, name: '联盟广告'},
-              {value: 135, name: '视频广告'},
-              {value: 1548, name: '搜索引擎'}
-            ]
-          }
-        ]
+        series: [{
+          data: [
+            ...this.dataSource
+          ]
+        }]
       })
     }
   }
-  /*export default {
-    name: "pieChart"
-  }*/
 </script>
 
 <style lang="scss" scoped>
-
+  .pieChart {
+    width: 100vw;
+    height: calc((100vh - 264px) / 2);
+  }
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -5,28 +5,27 @@
         <div class="totalSection">
           <div class="totalAmount">
             <div class="totalNumber">￥{{balance.toFixed(2)}}</div>
-            <div class="totalType">7月结余</div>
+            <div class="totalType">结余</div>
           </div>
         </div>
         <div class="detailSection">
           <div class="detail">
             <div class="detailNumber">￥{{spendingNumber.toFixed(2)}}</div>
-            <div class="detailType">7月支出</div>
+            <div class="detailType">支出</div>
           </div>
           <div class="detail">
             <div class="detailNumber">￥{{incomeNumber.toFixed(2)}}</div>
-            <div class="detailType">7月收入</div>
+            <div class="detailType">收入</div>
           </div>
         </div>
       </div>
-      <PieChart v-if="type === '+'"></PieChart>
-      <PieChart v-else>111</PieChart>
+
+      <PieChart :dataSource="type === '-' ? spendingPieChartArray : incomePieChartArray"></PieChart>
+
       <Types :defaultType.sync="type"></Types>
-      <LineChart v-if="type === '+'"></LineChart>
-      <LineChart v-else>111</LineChart>
-      {{spending}}
-      <hr>
-      {{income}}
+
+      <LineChart v-if="type === '-'"></LineChart>
+      <LineChart v-else></LineChart>
     </template>
   </Layout>
 </template>
@@ -35,39 +34,77 @@
   import Vue from 'vue'
   import {Component} from "vue-property-decorator";
   import Types from "@/components/money/Types.vue";
+
   @Component({
     components: {Types}
   })
-  export default class Statistics extends Vue{
-    type = '+';
+  export default class Statistics extends Vue {
+    type = '-';
+    incomePieChartArray = [{name: '', value: ''}];
+    spendingPieChartArray = [{name: '', value: ''}];
     recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]');
 
-    get spending(){
+    created() {
+      this.getSpendingData();
+      this.getIncomeData();
+    }
+
+    get spendingArray() {
       return this.recordList.filter((item: Record<string, string>) => {
         return item.type === '-';
-      }).map((item: Record<string, string>) => {
-        return Number(item.amount);
       })
     }
-    get spendingNumber(){
-      return this.spending.reduce((sum: number,item: number) => {
+
+    get spendingNumber() {
+      return this.spendingArray.map((item: Record<string, string>) => {
+        return Number(item.amount);
+      }).reduce((sum: number, item: number) => {
         return sum += item;
-      },0)
+      }, 0)
     }
-    get income(){
+
+    get incomeArray() {
       return this.recordList.filter((item: Record<string, string>) => {
         return item.type === '+';
-      }).map((item: Record<string, string>) => {
-        return Number(item.amount);
       })
     }
-    get incomeNumber(){
-      return this.income.reduce((sum: number,item: number) => {
+
+    get incomeNumber() {
+      return this.incomeArray.map((item: Record<string, string>) => {
+        return Number(item.amount);
+      }).reduce((sum: number, item: number) => {
         return sum += item;
-      },0)
+      }, 0)
     }
-    get balance(){
+
+    get balance() {
       return this.incomeNumber - this.spendingNumber;
+    }
+
+    getSpendingData() {
+      this.spendingArray.forEach((item: Record<string, string>) => {
+        const obj = {name: '', value: ''};
+        obj.name = item.tag;
+        obj.value = item.amount;
+        this.spendingPieChartArray.push(obj);
+      })
+      this.spendingPieChartArray.splice(0, 1);
+      if (this.spendingPieChartArray.length === 0){
+        this.spendingPieChartArray.push({name: '支出', value: '0'});
+      }
+    }
+
+    getIncomeData() {
+      this.incomeArray.forEach((item: Record<string, string>) => {
+        const obj = {name: '', value: ''};
+        obj.name = item.tag;
+        obj.value = item.amount;
+        this.incomePieChartArray.push(obj);
+      })
+      this.incomePieChartArray.splice(0, 1);
+      if (this.incomePieChartArray.length === 0){
+        this.incomePieChartArray.push({name: '收入', value: '0'});
+      }
     }
   }
 </script>
@@ -75,42 +112,50 @@
 <style lang="scss" scoped>
   @import "~@/assets/style/helper.scss";
 
-  .overview{
+  .overview {
     padding: 10px;
     color: white;
     height: 106px;
     background-color: $main-color;
     border-top: 1px solid #252628;
-    .totalSection{
+
+    .totalSection {
       display: flex;
       justify-content: center;
       align-items: center;
-      .totalAmount{
+
+      .totalAmount {
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        .totalNumber{
+
+        .totalNumber {
           font-size: 22px;
         }
-        .totalType{
+
+        .totalType {
           font-size: 12px;
         }
       }
     }
-    .detailSection{
+
+    .detailSection {
       display: flex;
       justify-content: space-around;
-      .detail{
+
+      .detail {
         padding-top: 6px;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        .detailNumber{
+
+        .detailNumber {
           font-size: 17px;
         }
-        .detailType{
+
+        .detailType {
           font-size: 12px;
         }
       }
