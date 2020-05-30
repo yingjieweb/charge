54ebcccb9ddd1506e75a7c121ccd3187a349b2ab
yingjieweb <1184061078@qq.com>
2020-05-30 11:19:58 +0800

    svg loader

--- vue.config.js
+++ vue.config.js
@@ -6,17 +6,17 @@ module.exports = {
       : '/',
   lintOnSave: false,
   chainWebpack: config =>{
-    const dir = path.resolve(__dirname, 'src/assets/icons')
+    const dir = path.resolve(__dirname, 'src/assets/icons') //svg图标的路径
 
-    config.module
-        .rule('svg-sprite')
-        .test(/\.svg$/)
-        .include.add(dir).end() // 包含 icons 目录
-        .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
+    config.module //vue将webpack封装后暴露出来的config接口
+        .rule('svg-sprite') //添加规则
+        .test(/\.svg$/)  //匹配.svg为结尾的文件
+        .include.add(dir).end() // 只包含 icons 目录的路径
+        .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()  //不需要解析成文件
         //.use('svgo-loader').loader('svgo-loader')  //删除svg中的fill填充色、npm install --dev svgo-loader一下（没装）
         //.tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))  //删除svg中的fill填充色
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
-    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
+    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录：loader 分功避免冲突
 
     // config.module
     //   .rule('svg-sprite')
