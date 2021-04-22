5dbb555401adc28ae0aa50ecbe471a5b06421b64
yingjieweb <1184061078@qq.com>
2021-04-22 20:46:51 +0800

    'deploy'

---
+++ precache-manifest.74218ef3fa06a32f1bb9930a4be2b9d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5a7924673c2fa13fc1",
+    "url": "/charge/css/app.83a7ae35.css"
+  },
+  {
+    "revision": "ae52c04629d4c11a1a6099005b1a7a5f",
+    "url": "/charge/img/sorry.ae52c046.jpg"
+  },
+  {
+    "revision": "86123fa920ec7e426a290637d33c25be",
+    "url": "/charge/index.html"
+  },
+  {
+    "revision": "ba5a7924673c2fa13fc1",
+    "url": "/charge/js/app.6c6549f8.js"
+  },
+  {
+    "revision": "8120f3469f7dae87440f",
+    "url": "/charge/js/chunk-vendors.b5e48347.js"
+  },
+  {
+    "revision": "c8342e0e7e333126048cd145b97df236",
+    "url": "/charge/manifest.json"
+  },
+  {
+    "revision": "0583ec4728ea2481e2be5f61986204d3",
+    "url": "/charge/qrcode.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/charge/robots.txt"
+  }
+]);
